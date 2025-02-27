f318dbdbe7d423c1c62184025d42b5ed65cf10c8
Alan Moreno <amoreno@perceptivesensors.com>
2025-02-27 16:11:22 -0700

    updated models

--- app/models/Appointment.js
+++ app/models/Appointment.js
@@ -3,9 +3,9 @@ import User from '@/app/models/User'
 
 const appointmentSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User' },
-  date: { type: Date },
-  duration: { type: Number, min: 30, max: 90 },
-  price: { type: Number }
+  date: { type: Date, required: true },
+  duration: { type: Number, min: 30, max: 90, required: true },
+  price: { type: Number, required: true }
 })
 
 const Appointment = models.Appointment || model("Appointment", appointmentSchema);
--- app/models/User.js
+++ app/models/User.js
@@ -2,10 +2,11 @@ import { Schema, model, models } from 'mongoose';
 import Appointment from '@/app/models/Appointment';
 
 const userSchema = new Schema({
-  name: {type: String},
+  name: {type: String, required: true },
   email: {type: String, required: true, unique: true},
-  age: {type: Number, min: 18},
+  age: {type: Number, min: 18, required: true },
   gender: {type: String}, 
+  description: {type: String},
   appointments: [{ type: Schema.Types.ObjectId, ref: 'Appointment' }]
 })
 
