747f235ee422fda019a30167dfa765378f7b3e93
Alan Moreno <amoreno@perceptivesensors.com>
2025-03-11 14:00:04 -0700

    date fns using current month; inc month

--- app/schedule/page.js
+++ app/schedule/page.js
@@ -5,12 +5,6 @@ const Schedule = async () => {
   return (
     <div className="flex flex-wrap m-auto"> 
       <Calendar />
-
-      {/* <div className="m-auto my-20 w-[500px] h-[700px] bg-slate-600 flex-wrap rounded-md">
-        <h1>
-          Pick a time that works for you
-        </h1>
-      </div> */}
     </div>
   );
 }
--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -1,17 +1,24 @@
 "use client"
+import axios from 'axios';
+import emailjs from '@emailjs/browser';
 import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { PageLoader } from '@/components/index';
-import emailjs from '@emailjs/browser';
 import { toast } from "sonner";
-import { format } from 'date-fns';
-import axios from 'axios';
+import { format, startOfToday, eachDayOfInterval, startOfMonth, endOfMonth, endOfWeek, isToday,
+  isSameMonth, isEqual, parse, add } from 'date-fns';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
 import { EllipsisVerticalIcon } from '@heroicons/react/24/outline'
 
 const Calendar = () => {
+  let today = startOfToday()
+  let [selectedDay, setSelectedDay] = useState(today)
+  let [currentMonth, setCurrentMonth] = useState(format(today, 'MMMM-yyyy'))
+  let firstDayCurrentMonth = parse(currentMonth, 'MMMM-yyyy', new Date())
+  let newDays = eachDayOfInterval({ start: startOfMonth(today), end: endOfWeek(endOfMonth(today)) })
+
   const router = useRouter();
   const [error, setError] = useState(null);
   const { data: session, status } = useSession();
@@ -25,50 +32,7 @@ const Calendar = () => {
     price: 50,
     userId: id
   });
-  const days = [
-    { date: '2021-12-27' },
-    { date: '2021-12-28' },
-    { date: '2021-12-29' },
-    { date: '2021-12-30' },
-    { date: '2021-12-31' },
-    { date: '2022-01-01', isCurrentMonth: true },
-    { date: '2022-01-02', isCurrentMonth: true },
-    { date: '2022-01-03', isCurrentMonth: true },
-    { date: '2022-01-04', isCurrentMonth: true },
-    { date: '2022-01-05', isCurrentMonth: true },
-    { date: '2022-01-06', isCurrentMonth: true },
-    { date: '2022-01-07', isCurrentMonth: true },
-    { date: '2022-01-08', isCurrentMonth: true },
-    { date: '2022-01-09', isCurrentMonth: true },
-    { date: '2022-01-10', isCurrentMonth: true },
-    { date: '2022-01-11', isCurrentMonth: true },
-    { date: '2022-01-12', isCurrentMonth: true, isToday: true },
-    { date: '2022-01-13', isCurrentMonth: true },
-    { date: '2022-01-14', isCurrentMonth: true },
-    { date: '2022-01-15', isCurrentMonth: true },
-    { date: '2022-01-16', isCurrentMonth: true },
-    { date: '2022-01-17', isCurrentMonth: true },
-    { date: '2022-01-18', isCurrentMonth: true },
-    { date: '2022-01-19', isCurrentMonth: true },
-    { date: '2022-01-20', isCurrentMonth: true },
-    { date: '2022-01-21', isCurrentMonth: true, isSelected: true },
-    { date: '2022-01-22', isCurrentMonth: true },
-    { date: '2022-01-23', isCurrentMonth: true },
-    { date: '2022-01-24', isCurrentMonth: true },
-    { date: '2022-01-25', isCurrentMonth: true },
-    { date: '2022-01-26', isCurrentMonth: true },
-    { date: '2022-01-27', isCurrentMonth: true },
-    { date: '2022-01-28', isCurrentMonth: true },
-    { date: '2022-01-29', isCurrentMonth: true },
-    { date: '2022-01-30', isCurrentMonth: true },
-    { date: '2022-01-31', isCurrentMonth: true },
-    { date: '2022-02-01' },
-    { date: '2022-02-02' },
-    { date: '2022-02-03' },
-    { date: '2022-02-04' },
-    { date: '2022-02-05' },
-    { date: '2022-02-06' },
-  ]
+
   const meetings = [
     {
       id: 1,
@@ -141,6 +105,11 @@ const Calendar = () => {
       console.log(err);
     }
   }
+  
+  const nextMonth = () => {
+    let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 })
+    setCurrentMonth(format(firstDayNextMonth, 'MMMM-yyyy'))
+  }
 
   function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -151,7 +120,9 @@ const Calendar = () => {
       <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
       <div className="md:pr-14">
         <div className="flex items-center">
-          <h2 className="flex-auto text-sm font-semibold text-gray-900">January 2022</h2>
+          <h2 className="flex-auto text-sm font-semibold text-gray-900">
+            {format(firstDayCurrentMonth, 'MMMM yyyy')}
+          </h2>
           <button
             type="button"
             className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
@@ -160,6 +131,7 @@ const Calendar = () => {
             <ChevronLeftIcon className="size-5" aria-hidden="true" />
           </button>
           <button
+            onClick={nextMonth}
             type="button"
             className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
           >
@@ -168,32 +140,35 @@ const Calendar = () => {
           </button>
         </div>
         <div className="mt-10 grid grid-cols-7 text-center text-xs/6 text-gray-500">
+          <div>S</div>
           <div>M</div>
           <div>T</div>
           <div>W</div>
           <div>T</div>
           <div>F</div>
           <div>S</div>
-          <div>S</div>
         </div>
         <div className="mt-2 grid grid-cols-7 text-sm">
-          {days.map((day, dayIdx) => (
-            <div key={day.date} className={classNames(dayIdx > 6 && 'border-t border-gray-200', 'py-2')}>
+          {newDays.map((day, dayIdx) => (
+            <div key={day.toString()} className={classNames(dayIdx > 6 && 'border-t border-gray-200', 'py-2')}>
               <button
+                onClick={() => setSelectedDay(day)}
                 type="button"
                 className={classNames(
-                  day.isSelected && 'text-white',
-                  !day.isSelected && day.isToday && 'text-indigo-600',
-                  !day.isSelected && !day.isToday && day.isCurrentMonth && 'text-gray-900',
-                  !day.isSelected && !day.isToday && !day.isCurrentMonth && 'text-gray-400',
-                  day.isSelected && day.isToday && 'bg-indigo-600',
-                  day.isSelected && !day.isToday && 'bg-gray-900',
-                  !day.isSelected && 'hover:bg-gray-200',
-                  (day.isSelected || day.isToday) && 'font-semibold',
+                  isEqual(day, selectedDay) && 'text-white',
+                  !isEqual(day, selectedDay) && isToday(day) && 'text-red-600',
+                  !isEqual(day, selectedDay) && !isToday(day) && isSameMonth(day, today) && 'text-gray-900',
+                  !isEqual(day, selectedDay) && !isToday(day) && !isSameMonth(day, today) && 'text-gray-400',
+                  isEqual(day, selectedDay) && isToday(day) && 'bg-red-600',
+                  isEqual(day, selectedDay) && !isToday(day) && 'bg-gray-900',
+                  !isEqual(day, selectedDay) && 'hover:bg-gray-200',
+                  (isEqual(day, selectedDay) || isToday(day)) && 'font-semibold',
                   'mx-auto flex size-8 items-center justify-center rounded-full',
                 )}
               >
-                <time dateTime={day.date}>{day.date.split('-').pop().replace(/^0/, '')}</time>
+                <time dateTime={format(day, 'yyyy-MM-dd')}>
+                  {format(day, 'd')}
+                </time>
               </button>
             </div>
           ))}
