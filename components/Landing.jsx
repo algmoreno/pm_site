18179e9aa36297b68fac5168dcf9d234b4cea0f2
Alan Moreno <amoreno@perceptivesensors.com>
2025-04-08 14:20:22 -0700

    word animation over sky

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -2,53 +2,95 @@
 import React from 'react'
 import * as THREE from "three"
 import "@/styles/globals.css"
-import { useRef } from "react"
+import { useState, useEffect, useRef } from "react"
+import { Html } from '@react-three/drei';
 import { Canvas, useFrame } from "@react-three/fiber"
 import { Clouds, Cloud, CameraControls, Sky as SkyImpl, StatsGl } from "@react-three/drei"
 import { useControls } from "leva"
 
 const Landing = () => {
   return (
-    <div id="canvas-container" className="w-full h-[700px] max-sm:mt-[25%] ">
+    <div id="canvas-container" className={`flex w-full h-[700px] max-sm:mt-[25%] text-center `}>
       <Canvas camera={{ position: [10, -10, 10], fov: 45 }}>
-        {/* <StatsGl /> */}
         <Sky />
+        <Words />
         <ambientLight intensity={Math.PI / 1.5} />
         <spotLight position={[0, 40, 0]} decay={0} distance={45} penumbra={1} intensity={100} />
         <spotLight position={[-20, 0, 10]} color="white" angle={0.15} decay={0} penumbra={-1} intensity={30} />
         <spotLight position={[20, -10, 10]} color="white" angle={0.2} decay={0} penumbra={-1} intensity={20} />
-        {/* <CameraControls /> */}
       </Canvas>
     </div>
   )
 }
 
+const Words = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [fontSize, setFontSize] = useState("text-[60px]");
+  const words = ['This', 'World', 'Moves', 'Fast', 'Slow', 'Down', 'And', 'Look', 'Around'];
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [show, setShow] = useState(true);
+
+  function randomizeXY() {
+    const minCeiled = Math.ceil(-6);
+    const maxFloored = Math.floor(6);
+    setX(Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)); 
+    setY(Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)); 
+  }
+
+  const sizes = ['text-[40px]', 'text-[50px]', 'text-[60px]', 'text-[70px]', 'text-[80px]'];
+  const randomizeFontSize = () => {
+    const size = sizes[Math.floor(Math.random() * sizes.length)];
+    setFontSize(size);
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const animate = async () => {
+      while (isMounted) {
+        setShow(true); // fade in
+        await new Promise((res) => setTimeout(res, 4000)); // stay visible
+        setShow(false); // fade out
+        await new Promise((res) => setTimeout(res, 4000)); // wait for fade out
+        randomizeFontSize();
+        randomizeXY();
+        setCurrentWordIndex((prev) => (prev + 1) % words.length); // switch word
+      }
+    };
+    
+    animate();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return (
+    <Html position={[x, y, 7]} className={`fixed z-100 word ${show ? 'fade-in' : 'fade-out'} text-center text-black ${fontSize}`}>
+        {words[currentWordIndex]}
+    </Html>
+  )
+}
+
+
+
+
 function Sky() {
   const ref = useRef()
   const cloud0 = useRef();
-  // const { color, x, y, z, range, ...config } = useControls({
-  //   seed: { value: 1, min: 1, max: 100, step: 1 },
-  //   segments: { value: 20, min: 1, max: 80, step: 1 },
-  //   volume: { value: 6, min: 0, max: 100, step: 0.1 },
-  //   opacity: { value: 0.8, min: 0, max: 1, step: 0.01 },
-  //   fade: { value: 10, min: 0, max: 400, step: 1 },
-  //   growth: { value: 4, min: 0, max: 20, step: 1 },
-  //   speed: { value: 0.1, min: 0, max: 10, step: 0.01 },
-  //   x: { value: 6, min: 0, max: 100, step: 1 },
-  //   y: { value: 1, min: 0, max: 100, step: 1 },
-  //   z: { value: 1, min: 0, max: 100, step: 1 },
-  //   color: "white",
-  // })
 
   useFrame((state, delta) => {
-    // ref.current.rotation.y = Math.cos(state.clock.elapsedTime / 2) / 2
-    // ref.current.rotation.x = Math.sin(state.clock.elapsedTime / 2) / 2
     cloud0.current.rotation.x -= .001
-    // cloud0.current.rotation.y -= .00001
   })
   return (
     <>
-      <SkyImpl />
+      <SkyImpl 
+        sunPosition={[100, 10, 100]} 
+        turbidity={10}
+        rayleigh={.5}
+        mieCoefficient={0.001}
+        mieDirectionalG={0.8}/>
       <group ref={ref}>
         <Clouds material={THREE.MeshLambertMaterial} limit={400} range="500">
           <Cloud ref={cloud0} speed=".2" bounds={[6, 1, 1]} color="#ffffff" position={[-15, 0, 0]} />
@@ -56,7 +98,7 @@ function Sky() {
           {/* <Cloud {...config} bounds={[x, y, z]} color="#d0e0d0" seed={3} position={[-15, 0, 0]} /> */}
           <Cloud bounds={[6, 1, 1]} color="#a0b0d0" seed={4} position={[0, 0, -12]} />
           <Cloud bounds={[6, 1, 1]} color="#c0c0dd" seed={5} position={[0, 0, 12]} />
-          <Cloud concentrate="outside" growth={100} color="#ffccdd" opacity={1.25} seed={0.3} bounds={200} volume={200} />
+          <Cloud concentrate="outside" growth={100} color="#ffccdd" opacity={1.25} seed={0.3} bounds={200} volume={150} />
         </Clouds>
       </group>
     </>
