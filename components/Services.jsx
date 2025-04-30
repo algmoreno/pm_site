28246209f7bcc751a34e52ca28d70fc9332f345f
Alan Moreno <amoreno@perceptivesensors.com>
2025-04-30 11:03:07 -0700

    service cards mobile styling, drop shadow

--- components/Services.jsx
+++ components/Services.jsx
@@ -52,8 +52,8 @@ const YogaSlide = forwardRef(function Slide({ content, color }, ref) {
 })
 
 const YogaTherapyCard = () => {
-  const serviceDiv = "w-full min-h-[400px] flex flex-col md:flex-row justify-center rounded-[10px] border border-blue-200";
-  const serviceSpan = "flex md:w-1/2 w-full justify-center rounded-[10px] text-right text-[20px] font-light p-3 backdrop-blur-lg backdrop-brightness-[90%]";
+  const serviceDiv = "w-full min-h-[400px] flex flex-col md:flex-row justify-center rounded-[10px] border border-white drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]";
+  const serviceSpan = "flex md:w-1/2 w-full justify-center md:rounded-l-[10px] sm:max-md:rounded-t-[10px] text-right text-[20px] font-light p-3 backdrop-blur-lg backdrop-brightness-[90%]";
   const serviceHeaderDiv = "";
 
   const ytSlides = ["Yoga therapy is a mind-body practice that focuses on your physical, emotional and mental health. The practice uses movement, mindfulness, meditation, relaxation and breathing exercises to help you relax, relieve stress and manage underlying conditions or symptoms in addition to treatment by a healthcare provider.", 
@@ -80,7 +80,7 @@ const YogaTherapyCard = () => {
           <MdKeyboardArrowRight size={20} className='text-white' />
         </div>
       </span>
-      <div className={`flex-wrap text-[60px] md:w-1/2 p-2 md:border-r text-center my-auto`}>
+      <div className={`flex-wrap text-5xl md:w-1/2 p-2 md:border-r text-center my-auto`}>
         <h1 className="yoga-therapy-service-h1 opacity-90 ">Yoga Therapy</h1>
       </div>
     </div>
@@ -88,8 +88,8 @@ const YogaTherapyCard = () => {
 }
 
 const YogaCard = () => {
-  const serviceDiv = "w-full min-h-[400px] flex flex-col md:flex-row justify-center rounded-[10px] border border-blue-200";
-  const serviceSpan = "flex md:w-1/2 w-full justify-center rounded-[10px] text-right text-[20px] font-light p-3 backdrop-blur-lg backdrop-brightness-[120%]";
+  const serviceDiv = "w-full min-h-[400px] flex flex-col md:flex-row justify-center rounded-[10px] border border-white drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]";
+  const serviceSpan = "flex md:w-1/2 w-full justify-center md:rounded-r-[10px] sm:max-md:rounded-b-[10px] text-right text-[20px] font-light p-3 backdrop-blur-lg backdrop-brightness-[120%]";
   const serviceHeaderDiv = "";
 
   const yogaSlides = ["Yoga is a holistic practice that combines physical postures (asanas), breath control (pranayama), and meditation (dhyana) to connect the body, mind, and spirit. It aims to create a sense of balance and well-being by harmonizing these aspects. Yoga is often described as a union, or yoking together, of these three elements.", 
@@ -105,7 +105,7 @@ const YogaCard = () => {
 
   return (
     <div className={`yoga-service-div ${serviceDiv}`}>
-      <div className={`flex-wrap text-[60px] md:w-1/2 p-2 md:border-l text-center my-auto`}>
+      <div className={`flex-wrap text-5xl md:w-1/2 p-2 md:border-l text-center my-auto`}>
         <h1 className={`yoga-service-h1 opacity-90`}>Yoga</h1>
       </div>
       <span className={`${serviceSpan} text-black relative`}>
